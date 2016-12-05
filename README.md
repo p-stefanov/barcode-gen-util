4fe3073ea8b4e92a4fa5d1bd29cc7c6abcd5f01c
Peter Stefanov <p.stefanov@hotmail.com>
2016-12-05 14:11:14 +0200

    Allow CR and stuff

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #start:
-carton exec 'starman --l :3000 /home/peter/playground/barcode/bin/app.pl --daemonize --pid /tmp/barcode.pid --workers 10'
+carton exec 'starman --l 127.0.0.1:3001 /home/peter/playground/barcode/bin/app.pl --daemonize --pid /tmp/barcode.pid --workers 10'
 
 #kill:
 cat /tmp/barcode.pid | xargs kill
--- bin/app.pl
+++ bin/app.pl
@@ -7,12 +7,14 @@ use Template;
 use Barcode::Code128;
 use GD;
 
-set 'template'     => 'template_toolkit';
-set 'logger'       => 'console';
-set 'log'          => 'debug';
-set 'show_errors'  => 1;
-set 'startup_info' => 1;
-set 'warnings'     => 1;
+set 'port'          => 3001;
+set 'template'      => 'template_toolkit';
+set 'logger'        => 'console';
+set 'log'           => 'debug';
+set 'show_errors'   => 1;
+set 'startup_info'  => 1;
+set 'warnings'      => 1;
+set 'server_tokens' => 0;
 
 get '/' => sub {
 	return template 'index.html';
@@ -21,22 +23,28 @@ get '/' => sub {
 post '/' => sub {
 	my $codes = [];
 	foreach (grep { $_ } split ',', param('codes')) {
-		my ($k, $v) = map { s/[\s;"']//gr } split '/', $_;
-		my $show = $v ? "$k ($v)" : $k;
+		my ($k, $v) = map { s/[^\\a-zA-Z0-9_-]//gr } split '/', $_;
+		#my $show = $v ? "$k ($v)" : $k;
+		#my $show = $v || $k;
+		my $show = $v || '';
 		push @$codes, { key => $k, val => $show };
 	}
+
 	return template 'codes.html', { codes => $codes };
 };
 
 my $bc = new Barcode::Code128;
 $bc->option('border', 0);
-$bc->option('show_text', 0);
+#$bc->option('show_text', 0);
+$bc->option('show_text', 1);
 $bc->option('height', 50);
 $bc->option('font_align', 'center');
 
 get '/code/:code' => sub {
 	content_type 'image/png';
-	return $bc->png(param('code'));
+	# s/\\(r|t|n)/qq{"\\$1"}/geer
+	my $code = ( param('code') =~ s/\\r/\r/gr ) =~ s/\\//gr;
+	return $bc->png($code);
 };
- 
+
 dance;
